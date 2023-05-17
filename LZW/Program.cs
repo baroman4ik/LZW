1768b8b028bdc205452f88a36d4e07036a4ce330
baroman4ik <baroman4i@mail.ru>
2023-05-17 17:57:55 +0300

    add tests

--- LZW/Program.cs
+++ LZW/Program.cs
@@ -1,122 +1,140 @@
-﻿using System.Collections.Generic;
-using System.IO;
-using System;
-using System.Text;
+﻿    using System.Collections.Generic;
+    using System.IO;
+    using System;
+    using System.Text;
+    using System.Diagnostics;
 
 class Program
-{
-    static void Main(string[] args)
     {
-        string inputFileName = "/Users/rom/Desktop/Study/LZW/LZW/input.txt";
-        string compressedFileName = "/Users/rom/Desktop/Study/LZW/LZW/compressed.lzw";
-        string decompressedFileName = "/Users/rom/Desktop/Study/LZW/LZW/decompressed.txt";
+        static void Main(string[] args)
+        {
+            string inputFileName = "/Users/rom/Study/LZW/LZW/input.txt";
+            string compressedFileName = "/Users/rom/Study/LZW/LZW/compressed.lzw";
+            string decompressedFileName = "/Users/rom/Study/LZW/LZW/decompressed.txt";
+
+            LZWCompressionPerformanceTest.Run(10, 10000);
 
         // Чтение данных из файла
         string inputText = File.ReadAllText(inputFileName, Encoding.Unicode);
-        byte[] inputData = Encoding.Unicode.GetBytes(inputText);
+            byte[] inputData = Encoding.Unicode.GetBytes(inputText);
 
-        // Сжатие данных методом LZW
-        byte[] compressedData = LZWCompression.Compress(inputData);
+            // Сжатие данных методом LZW
+            byte[] compressedData = LZWCompression.Compress(inputData);
 
-        Console.WriteLine(compressedData);
+            // Запись сжатых данных в файл
+            File.WriteAllBytes(compressedFileName, compressedData);
 
-        // Запись сжатых данных в файл
-        File.WriteAllBytes(compressedFileName, compressedData);
+            // Чтение сжатых данных из файла
+            byte[] compressedInputData = File.ReadAllBytes(compressedFileName);
 
-        // Чтение сжатых данных из файла
-        byte[] compressedInputData = File.ReadAllBytes(compressedFileName);
+            // Распаковка сжатых данных методом LZW
+            byte[] decompressedData = LZWCompression.Decompress(compressedInputData);
 
-        // Распаковка сжатых данных методом LZW
-        byte[] decompressedData = LZWCompression.Decompress(compressedInputData);
+            // Запись распакованных данных в файл
+            string decompressedText = Encoding.Unicode.GetString(decompressedData);
+            File.WriteAllText(decompressedFileName, decompressedText, Encoding.Unicode);
 
-        // Запись распакованных данных в файл
-        string decompressedText = Encoding.Unicode.GetString(decompressedData);
-        File.WriteAllText(decompressedFileName, decompressedText, Encoding.Unicode);
-
-        Console.WriteLine("Compression and decompression completed successfully!");
+            Console.WriteLine("Compression and decompression completed successfully!");
+        }
     }
+
+
+    public static class LZWCompression
+    {
+        // Метод сжатия данных методом LZW
+        public static byte[] Compress(byte[] data)
+        {
+            // Создание словаря с символами ASCII кодов от 0 до 255
+            var dictionary = new Dictionary<string, int>();
+            for (int i = 0; i < 256; i++)
+            {
+                dictionary.Add(((char)i).ToString(), i);
+            }
+
+            // Создание списка для хранения сжатых данных
+            var compressedData = new List<int>();
+            var word = ((char)data[0]).ToString();
+            for (int i = 1; i < data.Length; i++)
+            {
+                var c = (char)data[i];
+                var wc = word + c;
+                if (dictionary.ContainsKey(wc))
+                {
+                    word = wc;
+                }
+                else
+                {
+                    compressedData.Add(dictionary[word]);
+                    dictionary.Add(wc, dictionary.Count);
+                    word = c.ToString();
+                }
+            }
+
+            compressedData.Add(dictionary[word]);
+
+            // Преобразование списка сжатых данных в массив байтов
+            // и возврат результата
+            var compressedBytes = new List<byte>();
+            for (int i = 0; i < compressedData.Count; i++)
+            {
+                compressedBytes.AddRange(
+                    System.BitConverter.GetBytes(compressedData[i])
+                    );
+            }
+
+            return compressedBytes.ToArray();
+        }
+
+        // Метод разархивирования данных LZW
+        public static byte[] Decompress(byte[] data)
+        {
+            // Создание словаря с символами ASCII кодов от 0 до 255
+            var dictionary = new Dictionary<int, string>();
+            for (int i = 0; i < 256; i++)
+            {
+                dictionary.Add(i, ((char)i).ToString());
+            }
+
+            // Создание списка для хранения сжатых данных
+            var compressedData = new List<int>();
+            for (int i = 0; i < data.Length; i += 4)
+            {
+                compressedData.Add(System.BitConverter.ToInt32(data, i));
+            }
+
+            // Создание списка для хранения распакованных данных
+            var decompressedData = new List<string>();
+            var w = dictionary[compressedData[0]];
+            decompressedData.Add(w);
+            for (int i = 1; i < compressedData.Count; i++)
+            {
+                string entry = null;
+                if (dictionary.ContainsKey(compressedData[i]))
+                {
+                    entry = dictionary[compressedData[i]];
+                }
+                else if (compressedData[i] == dictionary.Count)
+                {
+                    entry = w + w[0];
+                }
+
+                decompressedData.Add(entry);
+                dictionary.Add(dictionary.Count, w + entry[0].ToString());
+                w = entry;
+            }
+
+            // Преобразование списка распакованных данных в массив байтов
+            // и возврат результата
+            var decompressedBytes = new List<byte>();
+            for (int i = 0; i < decompressedData.Count; i++)
+            {
+                decompressedBytes.AddRange(
+                    System.Text.Encoding.ASCII.GetBytes(decompressedData[i])
+                    );
+            }
+
+            return decompressedBytes.ToArray();
+        }
 }
 
 
-public static class LZWCompression
-{
-    public static byte[] Compress(byte[] data)
-    {
-        var dictionary = new Dictionary<string, int>();
-        for (int i = 0; i < 256; i++)
-        {
-            dictionary.Add(((char)i).ToString(), i);
-        }
-
-        var compressedData = new List<int>();
-        var word = ((char)data[0]).ToString();
-        for (int i = 1; i < data.Length; i++)
-        {
-            var c = (char)data[i];
-            var wc = word + c;
-            if (dictionary.ContainsKey(wc))
-            {
-                word = wc;
-            }
-            else
-            {
-                compressedData.Add(dictionary[word]);
-                dictionary.Add(wc, dictionary.Count);
-                word = c.ToString();
-            }
-        }
-
-        compressedData.Add(dictionary[word]);
-
-        var compressedBytes = new List<byte>();
-        for (int i = 0; i < compressedData.Count; i++)
-        {
-            compressedBytes.AddRange(System.BitConverter.GetBytes(compressedData[i]));
-        }
-
-        return compressedBytes.ToArray();
-    }
-
-    public static byte[] Decompress(byte[] data)
-    {
-        var dictionary = new Dictionary<int, string>();
-        for (int i = 0; i < 256; i++)
-        {
-            dictionary.Add(i, ((char)i).ToString());
-        }
-
-        var compressedData = new List<int>();
-        for (int i = 0; i < data.Length; i += 4)
-        {
-            compressedData.Add(System.BitConverter.ToInt32(data, i));
-        }
-
-        var decompressedData = new List<string>();
-        var w = dictionary[compressedData[0]];
-        decompressedData.Add(w);
-        for (int i = 1; i < compressedData.Count; i++)
-        {
-            string entry = null;
-            if (dictionary.ContainsKey(compressedData[i]))
-            {
-                entry = dictionary[compressedData[i]];
-            }
-            else if (compressedData[i] == dictionary.Count)
-            {
-                entry = w + w[0];
-            }
-
-            decompressedData.Add(entry);
-            dictionary.Add(dictionary.Count, w + entry[0].ToString());
-            w = entry;
-        }
-
-        var decompressedBytes = new List<byte>();
-        for (int i = 0; i < decompressedData.Count; i++)
-        {
-            decompressedBytes.AddRange(System.Text.Encoding.ASCII.GetBytes(decompressedData[i]));
-        }
-
-        return decompressedBytes.ToArray();
-    }
-}
---
+++ LZW/Tests/EmptyClass.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+
+public static class LZWCompressionPerformanceTest
+{
+    public static void Run(int iterations, int dataSize)
+    {
+        var data = new byte[dataSize];
+        var rnd = new Random();
+        rnd.NextBytes(data);
+
+        var stopwatch = new Stopwatch();
+
+        // Замер производительности метода сжатия
+        stopwatch.Start();
+        for (int i = 0; i < iterations; i++)
+        {
+            var compressedData = LZWCompression.Compress(data);
+        }
+        stopwatch.Stop();
+        var compressionTime = stopwatch.Elapsed.TotalMilliseconds / iterations;
+
+        // Замер производительности метода распаковки
+        var compressedBytes = LZWCompression.Compress(data);
+        stopwatch.Reset();
+        stopwatch.Start();
+        for (int i = 0; i < iterations; i++)
+        {
+            var decompressedData = LZWCompression.Decompress(compressedBytes);
+        }
+        stopwatch.Stop();
+        var decompressionTime = stopwatch.Elapsed.TotalMilliseconds / iterations;
+
+        Console.WriteLine($"Data size: {dataSize} bytes, iterations: {iterations}");
+        Console.WriteLine($"Compression time: {compressionTime:F3} ms");
+        Console.WriteLine($"Decompression time: {decompressionTime:F3} ms");
+    }
+}
